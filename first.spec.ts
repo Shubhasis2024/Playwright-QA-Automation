769b0942807f4151587b29b82bf9cec3d254c4a8
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2026-02-21 13:55:26 +0530

    Add files via upload

--- first.spec.ts
+++ first.spec.ts
@@ -10,3 +10,14 @@ test('TC-01-Verify the page title', async ({ page }) => {
   await expect(page).toHaveTitle("Installation | Playwright");
 
 });
+
+test('TC-02-Verify the page Url', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/docs/intro');
+
+  let pageurl = await page.url();
+  console.log(pageurl);
+
+  await expect(page).toHaveURL(/playwright/);
+
+});
