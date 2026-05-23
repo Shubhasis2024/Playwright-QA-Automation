072764bc62a848815170eb2d637c1c238f62cf9d
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2026-05-23 13:39:13 +0530

    Commiting with new script

--- Input.test.ts
+++ Input.test.ts
@@ -15,10 +15,35 @@ import { test, expect, Locator } from "playwright/test";
 // });
 
 
-test('Serach all the country name',async  ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/");
-    const country:Locator=page.locator('#country');
-    await country.selectOption(['usa', 'india']);
-    await expect(country).toHaveValues([/usa/,/india/]);
+// test('Serach all the country name',async  ({page}) => {
+//     await page.goto("https://testautomationpractice.blogspot.com/");
+//     const country:Locator=page.locator('#country');
+//     await country.selectOption( 'india');
+//     await expect(country).toHaveValue('india');
   
+// });
+
+
+//Those code has been added today 23/05/26
+
+// test('Verify to click on gennder dropdown ',async ({page}) => {
+//     await page.goto("https://testautomationpractice.blogspot.com/");
+//     const gender:Locator=page.locator('#male');
+//     await gender.click();
+//      await expect(gender).toHaveValue('male');
+//     const gender2:Locator=page.locator('#female');
+//     await gender2.click();
+//      await expect(gender2).toHaveValue('female');
+//      await page.waitForTimeout(4000);
+// });
+
+test('Verify the Days secletion in page by user', async ({page}) => {
+     await page.goto("https://testautomationpractice.blogspot.com/");
+     const day1:Locator=page.locator('#sunday');
+     await day1.click();
+     await expect(day1).toHaveValue('sunday');
+     const day2:Locator=page.locator('#monday');
+     await day2.click();
+    await expect(day2).toHaveValue('monday');
+    await page.waitForTimeout(4000);
 });
