fe2a9ed99de125ff8d7da85cf929876d791e13bd
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2026-05-30 18:04:27 +0530

    Add files via upload

--- Input.test.ts
+++ Input.test.ts
@@ -1,41 +1,41 @@
 import { test, expect, Locator } from "playwright/test";
 
-// test("Verify the input box functionality of firstname", async ({ page }) => {
-//   await page.goto("https://testautomationpractice.blogspot.com/");
-//   const name: Locator = page.locator("#name");
-//   await expect(name).toBeEnabled();
-//   await expect(name).toBeVisible();
+test("Verify the input box functionality of firstname", async ({ page }) => {
+  await page.goto("https://testautomationpractice.blogspot.com/");
+  const name: Locator = page.locator("#name");
+  await expect(name).toBeEnabled();
+  await expect(name).toBeVisible();
 
-//   await name.fill("Admin user");
-//   const entervalue: string = await name.inputValue();
-//   console.log("I have entered for testing at name placeholder:", entervalue);
+  await name.fill("Admin user");
+  const entervalue: string = await name.inputValue();
+  console.log("I have entered for testing at name placeholder:", entervalue);
 
-//   expect(entervalue).toBe("Admin user"); //Test cas pass today 17/05/2026
-//   await page.waitForTimeout(4000);
-// });
+  expect(entervalue).toBe("Admin user"); //Test cas pass today 17/05/2026
+  await page.waitForTimeout(4000);
+});
 
 
-// test('Serach all the country name',async  ({page}) => {
-//     await page.goto("https://testautomationpractice.blogspot.com/");
-//     const country:Locator=page.locator('#country');
-//     await country.selectOption( 'india');
-//     await expect(country).toHaveValue('india');
+test('Serach all the country name',async  ({page}) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    const country:Locator=page.locator('#country');
+    await country.selectOption( 'india');
+    await expect(country).toHaveValue('india');
   
-// });
+});
 
 
 //Those code has been added today 23/05/26
 
-// test('Verify to click on gennder dropdown ',async ({page}) => {
-//     await page.goto("https://testautomationpractice.blogspot.com/");
-//     const gender:Locator=page.locator('#male');
-//     await gender.click();
-//      await expect(gender).toHaveValue('male');
-//     const gender2:Locator=page.locator('#female');
-//     await gender2.click();
-//      await expect(gender2).toHaveValue('female');
-//      await page.waitForTimeout(4000);
-// });
+test('Verify to click on gennder dropdown ',async ({page}) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    const gender:Locator=page.locator('#male');
+    await gender.click();
+     await expect(gender).toHaveValue('male');
+    const gender2:Locator=page.locator('#female');
+    await gender2.click();
+     await expect(gender2).toHaveValue('female');
+     await page.waitForTimeout(4000);
+});
 
 test('Verify the Days secletion in page by user', async ({page}) => {
      await page.goto("https://testautomationpractice.blogspot.com/");
@@ -51,12 +51,31 @@ test('Verify the Days secletion in page by user', async ({page}) => {
 
 
 
-test.only ('verify the days all select ',async({page}) => {
+test('verify the days all select ',async({page}) => {
      await page.goto("https://testautomationpractice.blogspot.com/");
-     const days:string[]=['Sunday','Monday','Wednesday'];
+     const days:string[]=['Sunday','Monday','Wednesday','tuesday','thursday'];
      const selectbox:Locator[]=days.map(index=>page.getByLabel(index));
     for( const index of selectbox)
         {
             await index.check();
+             await page.waitForTimeout(4000);
+            await index.uncheck();
          }
+         
+});
+//This script added on the 30/05/2026
+
+test('Verify any one day select case', async ({page}) => {
+        await page.goto("https://testautomationpractice.blogspot.com/");
+         const days:string[]=['Sunday','Monday','Wednesday','tuesday','thursday'];
+
+         const weekval:string="Monday"
+         for(const lable of days){
+            if(weekval==lable){
+               const selectday= page.getByLabel(lable);
+               selectday.check();
+               await expect(selectday).toBeChecked();
+            }
+         }
+  await page.waitForTimeout(4000);
 });
